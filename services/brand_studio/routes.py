7f8e8c480cafba8faa83d89cd610df161ebcea79
LittleCoderMae <mervisnkubo@gmail.com>
2025-12-27 19:07:05 +0100

    brand_studio complete

---
+++ services/brand_studio/routes.py
@@ -0,0 +1,29 @@
+# services/brand_studio/routes.py
+from flask import Blueprint, render_template
+
+brand_bp = Blueprint(
+    "brand_studio",
+    __name__,
+    url_prefix="/brand-studio",
+    template_folder="templates"
+)
+
+@brand_bp.route("/")
+def home():
+    return render_template("br_home.html")
+
+@brand_bp.route("/services")
+def services():
+    return render_template("br_services.html")
+
+@brand_bp.route("/portfolio")
+def portfolio():
+    return render_template("br_portfolio.html")
+
+@brand_bp.route("/process")
+def process():
+    return render_template("br_process.html")
+
+@brand_bp.route("/contact")
+def contact():
+    return render_template("br_contact.html")
